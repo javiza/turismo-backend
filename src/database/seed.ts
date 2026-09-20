45f36d3b538fefc1807d20b890b14e44ee14b5de
javiza <jonathan@gmail.com>
2026-09-20 13:22:30 -0300

    reparando base de datos y coneccion con seed

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -32,6 +32,10 @@ async function seed() {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+     ssl:
+    process.env.DB_SSL === 'true'
+      ? { rejectUnauthorized: false }
+      : false,
     entities: [User],
     synchronize: false,
   });
